3a0550ae23184f2fd7f577aa53e1060f6c8be3ee
Praveen Cherukuri <praveen_cherukuri@epam.com>
2026-07-20 16:53:34 +0530

    Updated Code

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #------------------------------------------VPC Module--------------------------------------------------------#
 module "vpc_main" {
-  source                              = "git::https://github.com/CHERUKURIPRAVEEN/aws-vpc-module.git?ref=v0.8.0"
+  source                              = "git::https://github.com/CHERUKURIPRAVEEN/aws-vpc-module.git?ref=v0.10.0"
   region                              = var.region
   vpc_name                            = var.vpc_name
   cidr_block                          = var.cidr_block
@@ -15,51 +15,51 @@ module "vpc_main" {
   tags                                = var.tags
 }
 
-module "sg_ssh" {
-  source = "git::https://github.com/CHERUKURIPRAVEEN/aws-sg-module.git?ref=v0.3.0"
+# module "sg_ssh" {
+#   source = "git::https://github.com/CHERUKURIPRAVEEN/aws-sg-module.git?ref=v0.3.0"
 
-  description       = "Security group for SSH access ${var.application}"
-  required_vpc_name = var.required_vpc_name
-  environment       = var.environment
-  application       = var.application
-  application_code  = var.application_code
-  environment_code  = var.environment_code
-  project           = var.project
-  owner             = var.owner
-  app_owner         = var.app_owner
-  tags              = var.tags
+#   description       = "Security group for SSH access ${var.application}"
+#   required_vpc_name = var.required_vpc_name
+#   environment       = var.environment
+#   application       = var.application
+#   application_code  = var.application_code
+#   environment_code  = var.environment_code
+#   project           = var.project
+#   owner             = var.owner
+#   app_owner         = var.app_owner
+#   tags              = var.tags
 
-  ingress_rules = var.ingress_rules
-  egress_rules  = var.egress_rules
+#   ingress_rules = var.ingress_rules
+#   egress_rules  = var.egress_rules
 
 
-}
-#------------------------------------------EC2 Instance--------------------------------------------------------#
-module "ec2_instance" {
-  source = "git::https://github.com/CHERUKURIPRAVEEN/aws-ec2-module.git?ref=v0.8.0"
+# }
+# #------------------------------------------EC2 Instance--------------------------------------------------------#
+# module "ec2_instance" {
+#   source = "git::https://github.com/CHERUKURIPRAVEEN/aws-ec2-module.git?ref=v0.8.0"
 
-  for_each = var.servers_details
+#   for_each = var.servers_details
 
-  ami_name                     = var.ami_name
-  application                  = var.application
-  environment                  = var.environment
-  environment_code             = var.environment_code
-  application_code             = var.application_code
-  instance_role                = var.instance_role
-  key_pair                     = each.value.key_pair
-  project                      = var.project
-  backup                       = var.backup
-  owner                        = var.owner
-  app_owner                    = var.app_owner
-  description                  = var.description
-  availability_zone            = each.value.availability_zone
-  user_data_template_name      = var.user_data_template_name
-  instance_type                = each.value.instance_type
-  os_disk_size                 = each.value.os_disk_size
-  required_vpc_name            = var.required_vpc_name
-  tags                         = var.tags
-  number_of_instances          = var.number_of_instances
-  required_public_subnet_name  = each.value.required_public_subnet_name
-  required_private_subnet_name = each.value.required_private_subnet_name
-  security_groups              = [module.sg_ssh.security_group_id]
-}
+#   ami_name                     = var.ami_name
+#   application                  = var.application
+#   environment                  = var.environment
+#   environment_code             = var.environment_code
+#   application_code             = var.application_code
+#   instance_role                = var.instance_role
+#   key_pair                     = each.value.key_pair
+#   project                      = var.project
+#   backup                       = var.backup
+#   owner                        = var.owner
+#   app_owner                    = var.app_owner
+#   description                  = var.description
+#   availability_zone            = each.value.availability_zone
+#   user_data_template_name      = var.user_data_template_name
+#   instance_type                = each.value.instance_type
+#   os_disk_size                 = each.value.os_disk_size
+#   required_vpc_name            = var.required_vpc_name
+#   tags                         = var.tags
+#   number_of_instances          = var.number_of_instances
+#   required_public_subnet_name  = each.value.required_public_subnet_name
+#   required_private_subnet_name = each.value.required_private_subnet_name
+#   security_groups              = [module.sg_ssh.security_group_id]
+# }
--- variables/dev.tfvars
+++ variables/dev.tfvars
@@ -1,7 +1,7 @@
 #------------------------------------------ Global Variables --------------------------------------------------------#
-project_name = "VEENTECH"
-application  = "veen-app"
-project      = "VEENTECH"
+project_name = "VT"
+application  = "vt-app"
+project      = "VT"
 backup       = "NonProd"
 owner        = "praveen.cherukuri@veen.com"
 app_owner    = "praveen.cherukuri@veen.com"
@@ -12,7 +12,7 @@ application_code = "TRAN"
 #------------------------------------------ VPC Variables --------------------------------------------------------#
 region                              = "us-east-1"
 environment                         = "Dev"
-vpc_name                            = "MANAGEMENT_VPC"
+vpc_name                            = "TRAN_VPC"
 cidr_block                          = "10.11.0.0/16"
 enable_dns_hostnames                = true
 private_dns_hostname_type_on_launch = "ip-name"
@@ -30,59 +30,59 @@ private_subnets = [
 ]
 
 #------------------------------------------ EC2 Variables --------------------------------------------------------#
-ami_name                     = "Jenkins-Server-Image"
-os_disk_size                 = 30
-instance_type                = "t3.micro"
-required_vpc_name            = "TRAN_VPC"
-availability_zone            = "us-east-1a"
-user_data_template_name      = "userdata"
-key_pair                     = "tf-key-pair"
-number_of_instances          = 1
-required_public_subnet_name  = "public"
-required_private_subnet_name = ""
-instance_role                = "V-EC2-SSM"
-servers_details = {
-  server1 = {
-    instance_type                = "t3.micro"
-    os_disk_size                 = 30
-    key_pair                     = "tf-key-pair"
-    required_public_subnet_name  = "public"
-    required_private_subnet_name = ""
-    availability_zone            = "us-east-1a"
-  }
-}
+# ami_name                     = "Jenkins-Server-Image"
+# os_disk_size                 = 30
+# instance_type                = "t3.micro"
+# required_vpc_name            = "MANAGEMENT_VPC"
+# availability_zone            = "us-east-1a"
+# user_data_template_name      = "userdata"
+# key_pair                     = "tf-key-pair"
+# number_of_instances          = 1
+# required_public_subnet_name  = "public"
+# required_private_subnet_name = ""
+# instance_role                = "V-EC2-SSM"
+# servers_details = {
+#   server1 = {
+#     instance_type                = "t3.micro"
+#     os_disk_size                 = 30
+#     key_pair                     = "tf-key-pair"
+#     required_public_subnet_name  = "public"
+#     required_private_subnet_name = ""
+#     availability_zone            = "us-east-1a"
+#   }
+# }
 
-#------------------------------------------ SG Variables --------------------------------------------------------#
-ingress_rules = [
-  {
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_ipv4   = "0.0.0.0/0"
-    description = "SSH access"
-  },
-  {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_ipv4   = "0.0.0.0/0"
-    description = "HTTP access"
-  },
-  {
-    protocol    = "tcp"
-    from_port   = 443
-    to_port     = 443
-    cidr_ipv4   = "0.0.0.0/0"
-    description = "HTTPS access"
-  }
-]
+# #------------------------------------------ SG Variables --------------------------------------------------------#
+# ingress_rules = [
+#   {
+#     protocol    = "tcp"
+#     from_port   = 22
+#     to_port     = 22
+#     cidr_ipv4   = "0.0.0.0/0"
+#     description = "SSH access"
+#   },
+#   {
+#     protocol    = "tcp"
+#     from_port   = 80
+#     to_port     = 80
+#     cidr_ipv4   = "0.0.0.0/0"
+#     description = "HTTP access"
+#   },
+#   {
+#     protocol    = "tcp"
+#     from_port   = 443
+#     to_port     = 443
+#     cidr_ipv4   = "0.0.0.0/0"
+#     description = "HTTPS access"
+#   }
+# ]
 
-egress_rules = [
-  {
-    protocol    = "-1"
-    from_port   = 0
-    to_port     = 0
-    cidr_ipv4   = "0.0.0.0/0"
-    description = "All outbound traffic"
-  }
-]
+# egress_rules = [
+#   {
+#     protocol    = "-1"
+#     from_port   = 0
+#     to_port     = 0
+#     cidr_ipv4   = "0.0.0.0/0"
+#     description = "All outbound traffic"
+#   }
+# ]
