b2edc1efbc72997979eca58d7415940565562b21
Praveen Cherukuri <praveen_cherukuri@epam.com>
2026-07-20 16:55:50 +0530

    Updated Code

--- main.tf
+++ main.tf
@@ -2,6 +2,10 @@
 module "vpc_main" {
   source                              = "git::https://github.com/CHERUKURIPRAVEEN/aws-vpc-module.git?ref=v0.10.0"
   region                              = var.region
+  application                         = var.application
+  application_code                    = var.application_code
+  environment                         = var.environment
+  environment_code                    = var.environment_code
   vpc_name                            = var.vpc_name
   cidr_block                          = var.cidr_block
   enable_dns_hostnames                = var.enable_dns_hostnames
@@ -9,7 +13,6 @@ module "vpc_main" {
   private_subnets                     = var.private_subnets
   private_dns_hostname_type_on_launch = var.private_dns_hostname_type_on_launch
   map_public_ip_on_launch             = var.map_public_ip_on_launch
-  environment                         = var.environment
   create_nat_gateway                  = var.create_nat_gateway
   single_nat_gateway                  = var.single_nat_gateway
   tags                                = var.tags
